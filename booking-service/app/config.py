9c6e2d591ed413bc761381bd7d72bf8f279b557b
Ayaan Khan <khanayaan.2003@gmail.com>
2025-03-02 01:41:11 +0500

    BAS ENOUGH FOR TODAY

--- booking-service/app/config.py
+++ booking-service/app/config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:minahil.ali117@localhost:5432/bookingdb')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:123456789@localhost/bookingdb')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', 'pyamqp://guest@localhost//')  # RabbitMQ URL
     CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND', 'rpc://')
-    SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')  # You should generate a proper secret key
+    SECRET_KEY = os.getenv('SECRET_KEY', '123465789')  # You should generate a proper secret key
