3872419a216883ccbcc79827b51c93149209477b
minahilali117 <manahilali117@gmail.com>
2025-05-11 23:41:42 +0500

    my branch: test

--- user-service/main.py
+++ user-service/main.py
@@ -33,7 +33,11 @@ logger = logging.getLogger("my_app_logger")
 app = FastAPI()
 
 instrumentator = Instrumentator()
-Instrumentator().instrument(app).expose(app)
+instrumentator.instrument(app)
+
+@app.on_event("startup")
+async def expose_metrics():
+    instrumentator.expose(app)
 
 from fastapi.staticfiles import StaticFiles
 app.mount("/static", StaticFiles(directory="static"), name="static")
